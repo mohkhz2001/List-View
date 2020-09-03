5c6a452214527ac0229328e7d2eb781c868475fb
MohammadKZ <MohammadKZ@MOHAMMADKZ.local>
2020-09-03 15:10:10 +0430

    init views and views controler functions added

--- app/src/main/java/com/example/list_view/MainActivity.java
+++ app/src/main/java/com/example/list_view/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        ViewsController();
+    }
+
+    private void initViews(){
+
+    }
+
+    private void ViewsController(){
+        
     }
 }
